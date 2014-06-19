250d69590a15274f9a218b7dbd50342eeceb21c5
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-06-19 16:54:45 +0200

    Revert "Sensor: Add RotationVectorSensor2"
    This reverts commit e4dd699426efcba5bcbe00fa66e50fdac4c70cfd.

--- services/sensorservice/SensorService.cpp
+++ services/sensorservice/SensorService.cpp
@@ -46,7 +46,6 @@
 #include "LinearAccelerationSensor.h"
 #include "OrientationSensor.h"
 #include "RotationVectorSensor.h"
-#include "RotationVectorSensor2.h"
 #include "SensorFusion.h"
 #include "SensorService.h"
 
@@ -154,12 +153,6 @@ void SensorService::onFirstRef()
                 if (orientationIndex >= 0) {
                     mUserSensorList.removeItemsAt(orientationIndex);
                 }
-            } else if (orientationIndex != -1) {
-                // If we don't have a gyro but have a orientation sensor from
-                // elsewhere, we can compute rotation vector from that.
-                // (Google Maps expects rotation vector sensor to exist.)
-
-                registerVirtualSensor( &RotationVectorSensor2::getInstance() );
             }
 
             // debugging sensor list
@@ -327,12 +320,6 @@ bool SensorService::threadLoop()
                 if (fusion.isEnabled()) {
                     for (size_t i=0 ; i<size_t(count) ; i++) {
                         fusion.process(event[i]);
-                    }
-                }
-                RotationVectorSensor2& rv2(RotationVectorSensor2::getInstance());
-                if (rv2.isEnabled()) {
-                    for (size_t i=0 ; i<size_t(count) ; i++) {
-                        rv2.process(event[i]);
                     }
                 }
                 for (size_t i=0 ; i<size_t(count) && k<minBufferSize ; i++) {
